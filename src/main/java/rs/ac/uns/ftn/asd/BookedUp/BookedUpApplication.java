d9accf52546dbf68007e8d4a77be93da26895bbe
anapoparic <anapoparic28@gmail.com>
2023-11-21 19:19:49 +0100

    Project initialized

---
+++ src/main/java/rs/ac/uns/ftn/asd/BookedUp/BookedUpApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.asd.BookedUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookedUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookedUpApplication.class, args);
+	}
+
+}
