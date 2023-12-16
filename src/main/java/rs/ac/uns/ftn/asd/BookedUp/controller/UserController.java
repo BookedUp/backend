a38bed99afbcc3cebd730be83885dd5e6d1dae7b
VesnaVasic <105176519+VesnaVasic@users.noreply.github.com>
2023-12-16 13:30:10 +0100

    Update UserController.java

--- src/main/java/rs/ac/uns/ftn/asd/BookedUp/controller/UserController.java
+++ src/main/java/rs/ac/uns/ftn/asd/BookedUp/controller/UserController.java
@@ -27,7 +27,6 @@ import java.util.stream.Collectors;
 @CrossOrigin
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
