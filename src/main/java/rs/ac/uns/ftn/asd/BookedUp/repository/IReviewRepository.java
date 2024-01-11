920bf584958c801649e389bcfcc2942820e28e2b
Vesna Vasic <vasic.vesna.06@gmail.com>
2024-01-11 16:14:11 +0100

    Changed function deleteReview to be logic

--- src/main/java/rs/ac/uns/ftn/asd/BookedUp/repository/IReviewRepository.java
+++ src/main/java/rs/ac/uns/ftn/asd/BookedUp/repository/IReviewRepository.java
@@ -15,13 +15,14 @@ public interface IReviewRepository extends JpaRepository<Review, Long> {
     @Query("SELECT r FROM Review r WHERE r.accommodation.id = :accommodationId")
     List<Review> findAllByAccommodationId(@Param("accommodationId") Long accommodationId);
 
-    @Query("SELECT r FROM Review r WHERE r.guest.id = :guestId AND r.accommodation IS NOT NULL")
+    @Query("SELECT r FROM Review r WHERE r.guest.id = :guestId AND r.accommodation IS NOT NULL AND r.isReviewActive = true")
     List<Review> findAllAccommodationReviewsByGuestId(@Param("guestId") Long guestId);
 
-    @Query("SELECT r FROM Review r WHERE r.guest.id = :guestId AND r.host IS NOT NULL")
+    @Query("SELECT r FROM Review r WHERE r.guest.id = :guestId AND r.host IS NOT NULL AND r.isReviewActive = true")
     List<Review> findAllHostReviewsByGuestId(@Param("guestId") Long guestId);
 
-    @Query("SELECT r FROM Review r WHERE r.guest.id = :guestId")
+    @Query("SELECT r FROM Review r WHERE r.guest.id = :guestId AND r.isReviewActive = true")
     List<Review> findAllByGuestId(@Param("guestId") Long guestId);
+
 
 }
